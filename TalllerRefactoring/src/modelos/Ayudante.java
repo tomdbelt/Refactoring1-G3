60799dba086635da003d508e8b372fa79d764e1b
Tommy Beltrán <tomdbelt@espol.edu.ec>
2020-08-13 02:58:21 -0500

    Feature Envy Solved

--- TalllerRefactoring/src/modelos/Ayudante.java
+++ TalllerRefactoring/src/modelos/Ayudante.java
@@ -9,25 +9,8 @@ public class Ayudante {
     Ayudante(Estudiante e){
     	est = e;
     }
-    public String getMatricula() {
-        return est.getMatricula();
-    }
-
-    public void setMatricula(String matricula) {
-        est.setMatricula(matricula);
-    }
-
-    //Getters y setters se delegan en objeto estudiante para no duplicar código
-    public String getNombre() {
-        return est.getNombre();
-    }
-
-    public String getApellido() {
-        return est.getApellido();
-    }
 
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
-
 
     //Método para imprimir los paralelos que tiene asignados como ayudante
     public void MostrarParalelos(){
