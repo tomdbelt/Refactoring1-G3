1fa92c58b22e753bf6c0b0e94f41b91b4ce16894
Tommy Beltrán <tomdbelt@espol.edu.ec>
2020-08-13 19:31:29 -0500

    Cambios en la clase Ayudante. Se ha aplicado más técnicas de refactorización

--- TalllerRefactoring/src/modelos/Ayudante.java
+++ TalllerRefactoring/src/modelos/Ayudante.java
@@ -1,15 +1,38 @@
 package modelos;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Ayudante {
-    protected Estudiante est;
-    public ArrayList<Paralelo> paralelos;
+    private Estudiante est;
+    private List<Paralelo> paralelos;
 
     Ayudante(Estudiante e){
     	est = e;
+        paralelos = new ArrayList<>();
     }
 
+    public Estudiante getEst() {
+        return est;
+    }
+
+    public void setEst(Estudiante est) {
+        this.est = est;
+    }
+
+    public List<Paralelo> getParalelos() {
+        return Collections.unmodifiableList(paralelos);
+    }
+
+    public boolean addParalelo(Paralelo paralelo) {
+        return paralelos.add(paralelo);
+    }
+    
+    public boolean removeParalelo(Paralelo paralelo){
+        return paralelos.remove(paralelo);
+    }
+    
     //Los paralelos se añaden/eliminan directamente del Arraylist de paralelos
 
     //Método para imprimir los paralelos que tiene asignados como ayudante
