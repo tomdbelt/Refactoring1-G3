84b0168b706366f0a5404f96fd2baf5e768a11cc
tyrerodr <tyronerodriguezmotato@gmail.com>
2020-08-13 19:44:21 -0500

    Code smell Extract SuperClass

--- TalllerRefactoring/src/modelos/Estudiante.java
+++ TalllerRefactoring/src/modelos/Estudiante.java
@@ -2,17 +2,10 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Estudiante{
+public class Estudiante extends Persona{
     //Informacion del estudiante
     public String matricula;
-    public String nombre;
-    public String apellido;
-    public String facultad;
-    public int edad;
-    public String direccion;
-    public String telefono;
-    public ArrayList<Paralelo> paralelos;
-    
+
     //Getter y setter de Matricula
 
     public String getMatricula() {
@@ -22,63 +15,8 @@ public class Estudiante{
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
-
-    //Getter y setter del Nombre
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
     
-    //Getter y setter del Apellido
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    
-    //Getter y setter de la Facultad
-    public String getFacultad() {
-        return facultad;
-    }
-
-    public void setFacultad(String facultad) {
-        this.facultad = facultad;
-    }
-    
-    //Getter y setter de la edad
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-    
-    //Getter y setter de la direccion
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-    
-    //Getter y setter del telefono
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-      
-    //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
+        //Calcula y devuelve la nota final contando examen, deberes, lecciones y talleres. El teorico y el practico se calcula por parcial.
     
     public double CalcularNota(Paralelo p, Notas notas){
         double nota=0;
--- TalllerRefactoring/src/modelos/Persona.java
+++ TalllerRefactoring/src/modelos/Persona.java
@@ -17,9 +17,73 @@ public class Persona {
     public String apellido;
     public int edad;
     public String direccion;
+    public String facultad;
     public String telefono;
     public ArrayList<Paralelo> paralelos;
 
+    public Persona(String nombre, String apellido, int edad, String direccion, String facultad, String telefono, ArrayList<Paralelo> paralelos) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.direccion = direccion;
+        this.facultad = facultad;
+        this.telefono = telefono;
+        this.paralelos = paralelos;
+    }
+    //Getter y setter del Nombre
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    //Getter y setter del Apellido
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    //Getter y setter de la Facultad
+    public String getFacultad() {
+        return facultad;
+    }
+
+    public void setFacultad(String facultad) {
+        this.facultad = facultad;
+    }
+    
+    //Getter y setter de la edad
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    //Getter y setter de la direccion
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    //Getter y setter del telefono
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
     @HotSpotIntrinsicCandidate
     public Persona() {
     }
